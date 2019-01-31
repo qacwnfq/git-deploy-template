3fbcc580eab8b2ab242f8b831db19a4d0ec7bd6d
fredrik <fredrik.jadebeck@gmail.com>
2019-01-31 20:37:55 +0100

    try fixing css loading

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: '/git-deploy-template/static',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/git-deploy-template',
 
     /**
